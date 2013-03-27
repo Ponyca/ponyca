8df9aabfebb39b9b76b00313189114ca5c0a6aaa
0xa <xa@xomg.net>
2013-03-27 22:08:38 +0100

    Add real server main()

--- server/main.cpp
+++ server/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include "common/net/server.h"
+#include "common/config.h"
+#include "common/logging.h"
+
+using namespace Ponyca;
 
 int main(int argc, char** argv) {
-	std::cout << "Hello Equestria!" << std::endl;
-	return 0;
+    Logger::SetLogger("core", coreLog);
+
+    coreLog.addBackend(std::shared_ptr<Log::AbstractBackend>(new Log::CoutBackend()));
+    coreLog.setLevel(Log::DEBUG);
+    coreLog.logRaw("-- Hello Equestria! --");
+    coreLog.logInfo("Starting server");
+
+    try {
+        Net::ServerRouter sr;
+        sr.listen();
+
+        // TODO: Display a nice CLI
+        std::cin.get();
+
+        coreLog.logRaw("-- exiting --");
+        sr.close();
+    }
+    catch (std::exception &e) {
+        std::cerr << "**** EXCEPTION ****" << std::endl;
+        std::cerr << e.what() << std::endl;
+    }
+
+    Logger::DeleteAllLoggers();
+    return 0;
 }
