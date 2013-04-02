737042b12a51a3e61ff552856efd7bc0b4b96949
0xa <xa@xomg.net>
2013-04-02 23:03:31 +0200

    Improve server main

--- server/main.cpp
+++ server/main.cpp
@@ -2,32 +2,66 @@
 #include "common/net/server.h"
 #include "common/config.h"
 #include "common/logging.h"
+#include "common/commandparser.h"
+#include "build/opcodes.h"
 
 using namespace Ponyca;
 
+void handleAwesome(Net::ServerRouter &server, Net::RemotePlayer &client, Net::AbstractPacket const &packet) {
+
+}
+
 int main(int argc, char** argv) {
-    Logger::SetLogger("core", coreLog);
-
-    coreLog.addBackend(std::shared_ptr<Log::AbstractBackend>(new Log::CoutBackend()));
-    coreLog.setLevel(Log::DEBUG);
-    coreLog.logRaw("-- Hello Equestria! --");
-    coreLog.logInfo("Starting server");
-
     try {
+        // Set up logging
+        Logger::SetLogger("core", coreLog);
+        coreLog.addBackend(std::shared_ptr<Log::AbstractBackend>(new Log::CoutBackend()));
+        coreLog.setLevel(Log::DEBUG);
+        coreLog.logRaw("-- Hello Equestria! --");
+        coreLog.logInfo("Starting server");
+
+        CommandParser parser;
+
+        // Run autoexec.cfg (if any)
+        parser.runFile("./autoexec.cfg");
+
+        // Run CLI args
+        std::string command;
+        for (int i=1; i<argc; i++) {
+            if (argv[i][0] == '+') {
+                parser.run(command);
+            } else {
+                command += argv[i];
+                command += " ";
+            }
+        }
+
+        // Initialize everything
         Net::ServerRouter sr;
         sr.listen();
 
-        // TODO: Display a nice CLI
-        std::cin.get();
+        sr.setPacketHandler(0x0000, handleAwesome);
 
+        // Listen for console commands
+        try {
+            std::string input;
+            while(true && std::cin.good()) {
+                std::cout << "> " << std::flush;
+                std::getline(std::cin, input);
+                parser.run(input);
+            }
+        }
+        catch (CommandParser::QuitError) {}
+
+        // Close everything, exit.
         coreLog.logRaw("-- exiting --");
         sr.close();
+        Logger::DeleteAllLoggers();
     }
     catch (std::exception &e) {
         std::cerr << "**** EXCEPTION ****" << std::endl;
         std::cerr << e.what() << std::endl;
     }
 
-    Logger::DeleteAllLoggers();
     return 0;
 }
