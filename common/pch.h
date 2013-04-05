10d038fe2a3b864d430a879a5e8602bb237ecd82
0xa <xa@xomg.net>
2013-04-05 20:14:09 +0200

    Add pch.h and PrecompiledHeader.cmake

---
+++ common/pch.h
@@ -0,0 +1,18 @@
+#include <asio.hpp>
+
+#include <cmath>
+#include <cstdarg>
+#include <cstdint>
+#include <cstdlib>
+#include <cstring>
+
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <queue>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <vector>
